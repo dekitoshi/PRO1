29575717a083b608fef01f6c134ec846c85e7318
Pol Sau <polsau@yahoo.es>
2022-05-30 18:41:51 +0200

    cambio archivos

---
+++ X69939_ca.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int time_lapse(int time_1, int time_2) {
+    if (time_2 - time_1 > 0) return time_2 - time_1;
+    else return 60 + (time_2 - time_1);
+}
+
+int main() {
+    int a, b, c;
+    bool no_fetch = false;
+    cin >> a >> b >> c;
+    int f = time_lapse(a, b);
+    
+    while  (c != -1) {
+        if (f == time_lapse(b, c)) {
+            a = b;
+            b = c;
+            f = time_lapse(a, b);
+        } else no_fetch = true;
+
+        cin >> c;
+    }
+
+    if (no_fetch) cout << 0 << endl;
+    else cout << f << endl;  
+}
